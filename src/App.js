c8ea001c05b5a2f25407148d57dc490cc5cbe175
unknown <Richard Stg>
2020-11-11 04:13:38 +0000

    commit to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "./shared/styles/app.scss";
+import Layout from "./shared/hoc/layout/Layout";
+import Auth from "./auth/pages/Auth";
+import UnauthorizedUser from "./user/pages/UnauthorizedUser";
+import Users from "./user/pages/Users";
+import Feed from "./feed/pages/Feed";
+import AuthorizedUser from "./user/pages/AuthorizedUser";
+import GuestBook from "./guestbook/pages/GuestBook";
+import * as actions from "./store/actions/index";
+
+const App = (props) => {
+  const { onAuthCheckState } = props;
+
+  const authorizedRoutes = (
+    <Switch>
+      <Route path="/" exact component={Auth} />
+      <Route path="/user/:userId" component={UnauthorizedUser} />
+      <Route path="/users" component={Users} />
+      <Route path="/profile" component={AuthorizedUser} />
+      <Route path="/feed" component={Feed} />
+      <Route path="/guest-book" component={GuestBook} />
+      <Redirect to="/profile" />
+    </Switch>
   );
-}
 
-export default App;
+  const unauthorizedRoutes = (
+    <Switch>
+      <Route path="/" exact component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  useEffect(() => {
+    onAuthCheckState();
+  }, [onAuthCheckState]);
+
+  return (
+    <Router
+      onUpdate={() => window.scrollTo(0, 0)}
+      // history={createBrowserHistory()}
+    >
+      <Layout authPage={!props.isLoggedIn}>
+        {props.isLoggedIn ? authorizedRoutes : unauthorizedRoutes}
+      </Layout>
+    </Router>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    token: state.auth.token,
+    isLoggedIn: !!state.auth.token,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuthCheckState: () => {
+      dispatch(actions.authCheckState());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
